08f532cb2f0d498d454f2649e3c8f1b9e377befe
Jakob Gierschmann <j.gierschmann@gmail.com>
2024-07-09 10:13:27 +0200

    Refactored Projection.h

--- Projection.h
+++ Projection.h
@@ -9,10 +9,12 @@
 namespace MapperLib{
 
 class Projection {
+public:
+    virtual ~Projection() = default;
     [[nodiscard]] virtual Matrix project(Matrix const& data) const = 0;
 };
 
-class CoordinatePlaneProjection : public  Projection{
+class CoordinatePlaneProjection final : public  Projection{
 public:
     explicit CoordinatePlaneProjection(std::vector<Dimension> dimensions);
     [[nodiscard]] Matrix project (Matrix const& data) const override;
