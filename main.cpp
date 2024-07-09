5592947dcf158729a6cd7156508de67300043256
Jakob Gierschmann <j.gierschmann@gmail.com>
2024-07-09 10:15:41 +0200

    Added more testing to main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <memory>
-
+#include "CechComplex.h"
 #include "SingleLinkage.h"
 #include "DataCover.h"
+#include "Mapper.h"
 #include "typedefs.h"
 #include "Projection.h"
 
 int main() {
     using namespace MapperLib;
     const Matrix points = {{1, 2}, {3, 4}, {5, 6}, {8, 8}, {9, 9}};
-    SingleLinkage clusterer(2,std::nullopt);
+    SingleLinkage clusterer(1,std::nullopt);
 
     auto clusters = clusterer.predict(points,{0,1,2,3,4});
 
-    for(int i = 0; i < clusters.size(); i++) {
+    for(size_t i = 0; i < clusters.size(); i++) {
         std::cout << "Cluster " << i << ": { ";
         for(auto const point_id: clusters[i]) {
             std::cout << "[" << points[point_id][0] << ", " << points[point_id][1] << "], ";
@@ -24,14 +25,26 @@ int main() {
     const Matrix data = {{1,2,3}, {2,3,4}, {3,4,5}, {5,6,7}, {6,7,8}, {6,7,8},{7,8,9},{8,9,10}};
 
 
-    DataCover const cover(3,0.5,data);
+    DataCover const cover(2,0.5,data);
     for (size_t i = 0; i < cover.get_total_num_cubes(); i++){
         std::cout << i << ": " << cover.convert_to_linear_cube_id(cover.convert_to_cube_id(i)) << std::endl;
+        std::cout << cover.convert_to_cube_id(i) << std::endl;
     }
 
-    std::cout << cover.get_native_cube_id({9,9,9}) << std::endl;
 
-    auto projection = std::make_unique<Mapper::CoordinatePlaneProjection>(std::vector<size_t>({0,1}));
+    std::cout << cover.get_native_cube_id({8,9,9}) << std::endl;
+
+    std::unique_ptr<Clusterer> clusterer_ptr = std::make_unique<SingleLinkage>(1, std::nullopt);
+    auto data_cover_ptr = std::make_unique<DataCover>(2,0.5, data);
+    std::unique_ptr<Projection> projection_ptr = std::make_unique<CoordinatePlaneProjection>(std::vector<size_t>({0,1}));
+    std::unique_ptr<Complex> complex_ptr = std::make_unique<CechComplex>(*data_cover_ptr, 1);
+
+    Mapper mapper(std::move(clusterer_ptr), std::move(data_cover_ptr), std::move(projection_ptr), std::move(complex_ptr));
+    auto const simplices = mapper.map(data);
+    std::cout << "Simplices: " << std::endl;
+    for (auto const& simplex : simplices) {
+        std::cout << simplex << std::endl;
+    }
 
     return 0;
 }
